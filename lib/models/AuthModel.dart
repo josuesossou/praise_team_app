db719cfe2e1840ae7e1cdd06ac8f4ab0923bdaee
Josue Sossou <jmac@Josues-MacBook-Pro.local>
2021-06-24 21:57:12 -0400

    updating login page

--- lib/models/AuthModel.dart
+++ lib/models/AuthModel.dart
@@ -2,36 +2,38 @@ import 'package:flutter/foundation.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import '../services/dbUsersQuery.dart';
 
-class AuthModel with ChangeNotifier { 
+class AuthModel with ChangeNotifier {
   User authUser;
   String personalEmail;
   FirebaseAuth auth = FirebaseAuth.instance;
   DbUsers dbusers = DbUsers();
 
   AuthModel() {
-    auth.authStateChanges()
-      .listen((User user) {
-        if (user == null) {
-          authUser = null;
-        } else {
-          // check if email already exist
-          //no need to use local storage, save user info in the db
-          DbUsers().getUser(authUser.uid, personalEmail);
-          authUser = user;
-        }
-        notifyListeners();
-      });
+    auth.authStateChanges().listen((User user) {
+      if (user == null) {
+        authUser = null;
+      } else {
+        // check if email already exist
+        //no need to use local storage, save user info in the db
+        DbUsers().getUser(authUser.uid, personalEmail);
+        authUser = user;
+      }
+      notifyListeners();
+    });
   }
 
   Future<bool> doSigning(email, password, pEmail) async {
     personalEmail = pEmail;
 
-    UserCredential organizationInfo = await auth.signInWithEmailAndPassword(email: email, password: password);
+    UserCredential organizationInfo =
+        await auth.signInWithEmailAndPassword(email: email, password: password);
     bool isUser = await dbusers.checkUser(pEmail, organizationInfo.user.uid);
     // authUser.sendEmailVerification()
 
-    //add personal email to login view
+    // print(isUser);
+    // print(organizationInfo.user);
+
+    // add personal email to login view
     return isUser;
   }
-  
 }
--- lib/start/login.dart
+++ lib/start/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lgcogpraiseteam/models/AuthModel.dart';
 import '../components/flexText.dart';
 import '../components/textField.dart';
 import '../components/logo.dart';
@@ -10,17 +11,22 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
-  final SizedBox sizedBox = SizedBox(height: 30,);
-  Map<String, String> loginInfo = {};
+  final SizedBox sizedBox = SizedBox(
+    height: 30,
+  );
+  bool userExist;
+  Map<String, String> loginInfo = {
+    'orgEmail': '',
+    'orgPassword': '',
+    'personalEmail': ''
+  };
+  AuthModel auth = AuthModel();
 
   @override
   void initState() {
-    loginInfo = {
-      'orgEmail': '',
-      'orgPassword': '',
-      'personalEmail': ''
-    };
-
+    setState(() {
+      userExist = false;
+    });
     super.initState();
   }
 
@@ -28,33 +34,44 @@ class _LoginState extends State<Login> {
     loginInfo[type] = changedText;
   }
 
+  signin() {
+    auth
+        .doSigning(loginInfo['orgEmail'], loginInfo['orgPassword'],
+            loginInfo['personalEmail'])
+        .then((isUserFound) => {
+              setState(() {
+                userExist = isUserFound;
+              })
+            });
+  }
+
   @override
   Widget build(BuildContext context) {
-    ThemeData theme = Theme.of(context);
-
     return Scaffold(
       backgroundColor: Colors.lightBlueAccent,
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Logo(size: 30,),
+          Logo(
+            size: 30,
+          ),
           // sizedBox,
           Container(
-            height: MediaQuery.of(context).size.height * 0.6,
+            height: MediaQuery.of(context).size.height * 0.5,
             width: MediaQuery.of(context).size.width,
             child: LoginInfo(
-              updateLoginInfo: updateLoginInfo,    
+              updateLoginInfo: updateLoginInfo,
+              signin: signin,
             ),
           ),
           // Container(
           //   height: MediaQuery.of(context).size.height * 0.5,
           //   width: MediaQuery.of(context).size.width,
           //   child: AdditionalInfo(
-          //     updateLoginInfo: updateLoginInfo,    
+          //     updateLoginInfo: updateLoginInfo,
           //   ),
           // ),
-
         ],
       ),
     );
@@ -62,11 +79,17 @@ class _LoginState extends State<Login> {
 }
 
 class LoginInfo extends StatelessWidget {
-  LoginInfo({ @required this.updateLoginInfo });
+  LoginInfo({
+    @required this.updateLoginInfo,
+    @required this.signin,
+  });
 
   final Function updateLoginInfo;
+  final Function signin;
 
-  final SizedBox sizedBox = SizedBox(height: 30,);
+  final SizedBox sizedBox = SizedBox(
+    height: 30,
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -76,13 +99,11 @@ class LoginInfo extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         mainAxisAlignment: MainAxisAlignment.center,
-
         children: [
           FlexText(
             text: 'Login',
             alignment: Alignment.bottomCenter,
           ),
-
           sizedBox,
           FlexText(
             text: 'Oraganiztion Email',
@@ -90,25 +111,19 @@ class LoginInfo extends StatelessWidget {
           ),
           TextFieldCop(
             obscureText: false,
-            onChange: (text) => updateLoginInfo(
-              'orgEmail', text
-            ),
+            onChange: (text) => updateLoginInfo('orgEmail', text),
             color: theme.primaryColorDark,
           ),
-            
           sizedBox,
           FlexText(
             text: 'Oraganiztion Password',
-            margin: EdgeInsets.only(left: 25),          
+            margin: EdgeInsets.only(left: 25),
           ),
           TextFieldCop(
             obscureText: true,
-            onChange: (text) => updateLoginInfo(
-              'orgPassword', text
-            ),
+            onChange: (text) => updateLoginInfo('orgPassword', text),
             color: theme.primaryColorDark,
           ),
-
           sizedBox,
           FlexText(
             text: 'Personal Email',
@@ -116,12 +131,9 @@ class LoginInfo extends StatelessWidget {
           ),
           TextFieldCop(
             obscureText: true,
-            onChange:  (text) => updateLoginInfo(
-              'personalEmail', text
-            ),
+            onChange: (text) => updateLoginInfo('personalEmail', text),
             color: Theme.of(context).primaryColorLight,
           ),
-
           sizedBox,
           RectButton(
             margin: EdgeInsets.symmetric(horizontal: 20),
@@ -129,12 +141,10 @@ class LoginInfo extends StatelessWidget {
             color: Color(0xff01A0C7),
             padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
             child: Text('Login',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.white, fontWeight: FontWeight.bold
-              )
-            ), 
-            onPress: () {},
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: Colors.white, fontWeight: FontWeight.bold)),
+            onPress: () => signin(),
             width: MediaQuery.of(context).size.width,
           )
         ],
@@ -144,68 +154,61 @@ class LoginInfo extends StatelessWidget {
 }
 
 class AdditionalInfo extends StatelessWidget {
-  AdditionalInfo({ @required this.updateLoginInfo });
+  AdditionalInfo({@required this.updateLoginInfo});
 
   final Function updateLoginInfo;
 
-  final SizedBox sizedBox = SizedBox(height: 30,);
+  final SizedBox sizedBox = SizedBox(
+    height: 30,
+  );
 
   @override
   Widget build(BuildContext context) {
     ThemeData theme = Theme.of(context);
 
     return Container(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          FlexText(
-            text: 'Additional Info',   
-            alignment: Alignment.bottomCenter,
-          ),
-
-          sizedBox,
-          FlexText(
-            text: 'Name',
-            margin: EdgeInsets.only(left: 25),
-          ),
-          TextFieldCop(
-            obscureText: false,
-            onChange: (text) => updateLoginInfo(
-              'fullName', text
-            ),
-            color: theme.primaryColorDark,
-          ),
-
-          sizedBox,
-          FlexText(
-            text: 'Role eg. Drumer, Guitarist, Pianist ... (Optional)',
-            margin: EdgeInsets.only(left: 25),
-          ),
-          TextFieldCop(
-            obscureText: true,
-            onChange: (text) => updateLoginInfo(
-              'role', text
-            ),
-            color: Theme.of(context).primaryColorLight,
-          ),
-          sizedBox,
-          RectButton(
-            margin: EdgeInsets.symmetric(horizontal: 20),
-            elevation: 5.0,
-            color: Color(0xff01A0C7),
-            padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-            child: Text('Register',
+        child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        FlexText(
+          text: 'Additional Info',
+          alignment: Alignment.bottomCenter,
+        ),
+        sizedBox,
+        FlexText(
+          text: 'Name',
+          margin: EdgeInsets.only(left: 25),
+        ),
+        TextFieldCop(
+          obscureText: false,
+          onChange: (text) => updateLoginInfo('fullName', text),
+          color: theme.primaryColorDark,
+        ),
+        sizedBox,
+        FlexText(
+          text: 'Role eg. Drumer, Guitarist, Pianist ... (Optional)',
+          margin: EdgeInsets.only(left: 25),
+        ),
+        TextFieldCop(
+          obscureText: true,
+          onChange: (text) => updateLoginInfo('role', text),
+          color: Theme.of(context).primaryColorLight,
+        ),
+        sizedBox,
+        RectButton(
+          margin: EdgeInsets.symmetric(horizontal: 20),
+          elevation: 5.0,
+          color: Color(0xff01A0C7),
+          padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          child: Text('Register',
               textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.white, fontWeight: FontWeight.bold
-              )
-            ),
-            onPress: () {},
-            width: MediaQuery.of(context).size.width,
-          ),
-        ],
-      )
-    );
+              style:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+          onPress: () {},
+          width: MediaQuery.of(context).size.width,
+        ),
+      ],
+    ));
   }
 }
