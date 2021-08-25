bbf0e5b688e7c0afc485bab169cbe6f7fc841ca0
Josue Sossou <jmac@Josues-MacBook-Pro.local>
2021-08-24 22:36:00 -0400

    successfuly querry with contains

--- lib/main.dart
+++ lib/main.dart
@@ -80,13 +80,13 @@ class _SwitchNavigatorState extends State<SwitchNavigator> {
   bool _amplifyConfigured = false;
 
   void _configureAmplify() async {
-
-    // await Amplify.addPlugin(AmplifyAPI()); // UNCOMMENT this line after backend is deployed
-    await Amplify.addPlugin(AmplifyDataStore(modelProvider: ModelProvider.instance));
-
-    // Once Plugins are added, configure Amplify
-    await Amplify.configure(amplifyconfig);
     try {
+      // await Amplify.addPlugin(AmplifyAPI()); // UNCOMMENT this line after backend is deployed
+      await Amplify.addPlugin(AmplifyDataStore(modelProvider: ModelProvider.instance));
+
+      // Once Plugins are added, configure Amplify
+      await Amplify.configure(amplifyconfig);
+    
       setState(() {
         _amplifyConfigured = true;
       });
@@ -95,22 +95,23 @@ class _SwitchNavigatorState extends State<SwitchNavigator> {
     }
   }
 
+
+
+
   @override
   void initState() {
     super.initState();
     _configureAmplify();
   }
 
+
   @override
   Widget build(BuildContext context) {
     // final auth = Provider.of<AuthModel>(context);
     // myModel.doSomething();
     return _amplifyConfigured ? MaterialApp(
-
-    home: Scaffold(
-      appBar: AppBar(),
-      body: Container(color: Colors.lightBlue,),
-    ))
+      home: HomeScreen()
+    )
     // MaterialApp(
     //   theme: ThemeData(
     //     brightness: Brightness.light,
@@ -124,12 +125,229 @@ class _SwitchNavigatorState extends State<SwitchNavigator> {
     : MaterialApp(
       home: Container()
     );
-
-    
   }
 }
 
-final item = Todo(
-		name: "Lorem ipsum dolor sit amet",
-		description: "Lorem ipsum dolor sit amet");
-await Amplify.DataStore.save(item);
+// final item = Todo(
+// 		name: "Lorem ipsum dolor sit amet",
+// 		description: "Lorem ipsum dolor sit amet");
+// await Amplify.DataStore.save(item);
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({ Key key }) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  StreamSubscription _subscription;
+
+  List<Todo> _todos = [];
+
+  Future<void> _fetchTodos() async {
+    // to be filled in a later step
+    // _subscription = Amplify.DataStore.observe(Todo.classType).listen((event) {
+    //   _fetchTodos();
+    // });
+    try {
+    
+      // query for all Todo entries by passing the Todo classType to
+      // Amplify.DataStore.query();
+      List<Todo> updatedTodos = await Amplify.DataStore
+      .query(Todo.classType, where: Todo.NAME.contains('AND') );
+      
+      //update the ui state to reflect fetched todos
+      setState(() {
+        _todos = updatedTodos;
+      });
+    } catch (e) {
+      print('An error occurred while querying Todos: $e');
+    }
+  }
+
+    @override
+  void dispose() {
+    // to be filled in a later step
+    // _subscription.cancel();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    _fetchTodos();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        color: Colors.lightBlue,
+        child: TodosList(todos: _todos,)
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => AddTodoForm()),
+          );
+        },
+        tooltip: 'Add Todo',
+        label: Row(
+          children: [Icon(Icons.add), Text('Add todo')],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+}
+
+class TodosList extends StatelessWidget {
+  final List<Todo> todos;
+
+  TodosList({@required this.todos});
+
+  @override
+  Widget build(BuildContext context) {
+    return todos.isEmpty ?
+        Center(child: Text('Tap button below to add a todo!'))
+        : ListView(
+            padding: EdgeInsets.all(8),
+            children: todos.map((todo) => TodoItem(todo: todo)).toList());
+  }
+}
+
+
+
+class TodoItem extends StatelessWidget {
+  final double iconSize = 24.0;
+  final Todo todo;
+
+  TodoItem({@required this.todo});
+
+  void _deleteTodo(BuildContext context) async {
+    // to be filled in a later step
+     try {
+      // to delete data from DataStore, we pass the model instance to
+      // Amplify.DataStore.delete()
+      await Amplify.DataStore.delete(todo);
+    } catch (e) {
+      print('An error occurred while deleting Todo: $e');
+    }
+  }
+
+  Future<void> _toggleIsComplete() async {
+    // to be filled in a later step
+      // copy the Todo we wish to update, but with updated properties
+    // Todo updatedTodo = todo.copyWith(isComplete: !todo.isComplete);
+    // try {
+
+    //   // to update data in DataStore, we again pass an instance of a model to
+    //   // Amplify.DataStore.save()
+    //   await Amplify.DataStore.save(updatedTodo);
+    // } catch (e) {
+    //   print('An error occurred while saving Todo: $e');
+    // }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: InkWell(
+        onTap: () {
+          _toggleIsComplete();
+        },
+        onLongPress: () {
+          _deleteTodo(context);
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(children: [
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(todo.name,
+                      style:
+                        TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                  Text(todo.description ?? 'No description'),
+                ],
+              ),
+            ),
+            // Icon(
+            //     todo.isComplete
+            //         ? Icons.check_box
+            //         : Icons.check_box_outline_blank,
+            //     size: iconSize),
+          ]),
+        ),
+      ),
+    );
+  }
+}
+
+
+
+class AddTodoForm extends StatefulWidget {
+  @override
+  _AddTodoFormState createState() => _AddTodoFormState();
+}
+
+class _AddTodoFormState extends State<AddTodoForm> {
+  final _nameController = TextEditingController();
+  final _descriptionController = TextEditingController();
+
+  Future<void> _saveTodo() async {
+    // get the current text field contents
+    String name = _nameController.text;
+    String description = _descriptionController.text;
+
+    // create a new Todo from the form values
+    // `isComplete` is also required, but should start false in a new Todo
+    Todo newTodo = Todo(
+      name: name,
+      description: description.isNotEmpty ? description : null,
+    );
+
+    try {
+      // to write data to DataStore, we simply pass an instance of a model to
+      // Amplify.DataStore.save()
+      await Amplify.DataStore.save(newTodo);
+
+      // after creating a new Todo, close the form
+      Navigator.of(context).pop();
+    } catch (e) {
+      print('An error occurred while saving Todo: $e');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add Todo'),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(8.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              TextFormField(
+                  controller: _nameController,
+                  decoration: InputDecoration(filled: true, labelText: 'Name')),
+              TextFormField(
+                  controller: _descriptionController,
+                  decoration:
+                      InputDecoration(filled: true, labelText: 'Description')),
+              ElevatedButton(onPressed: _saveTodo, child: Text('Save'))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
